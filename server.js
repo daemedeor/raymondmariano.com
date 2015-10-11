a95573634af7874ffbf95b7a54aa1f8b5fc7dbaa
Kingtak Wong <k.just.wong@gmail.com>
2015-10-11 13:04:51 -1000

    adding in an active state

--- server.js
+++ server.js
@@ -12,8 +12,29 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+app.get("/contact", function(req,res){
+  res.render("contact", {selected: '/contact'});
+});
+
+app.get("/about", function(req,res){
+  res.render("about", {selected: '/about'});
+});
+
+app.get("/videos", function(req,res){
+  res.render("videos", {selected: '/videos'});
+});
+
+app.get("/gifs", function(req, res) {
+  res.render("gifs", {selected: '/gifs'});
+});
+
+app.get("/artportfolio", function(req, res) {
+  res.render("artportfolio", {selected: '/artportfolio'});
+});
+
 app.get("/", function(req, res) {
-    res.render("home");
+  res.render("home", {selected: '/'});
 });
 
 server.listen(app.get('port'), function(){
