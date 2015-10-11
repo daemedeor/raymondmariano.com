93205b10567fd9c37c7839c81474288bc067a21f
Kingtak Wong <k.just.wong@gmail.com>
2015-10-11 12:48:44 -1000

    adding a new menu

--- server.js
+++ server.js
@@ -3,14 +3,19 @@ var express = require('express')
     , path = require("path")
     , server  = require("http").createServer(app)
     , bodyParser = require("body-parser")
-    , config = require("../config/config");
+    , config = require("./config/config");
 
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
-app.use(express.static(path.join(__dirname, '../public')));
+app.set('port', process.env.PORT || config.portToListen);
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", function(req, res) {
-    res.render("views/home");
+    res.render("home");
+});
+
+server.listen(app.get('port'), function(){
+    console.log("started on part", app.get("port"));
 });
