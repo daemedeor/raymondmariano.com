f736af1fae26419bea632f2704740a49f293c997
Kingtak Wong <k.just.wong@gmail.com>
2015-10-11 12:04:17 -1000

    adding in foundation

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 var express = require('express')
     , app = express()
+    , path = require("path")
     , server  = require("http").createServer(app)
     , bodyParser = require("body-parser")
     , config = require("../config/config");
+
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+    res.render("views/home");
+});
