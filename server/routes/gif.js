78a427f5a644ff16f4ff496e17d622f92cdd6db9
Kingtak Wong <k.just.wong@gmail.com>
2015-10-13 01:26:42 -1000

    most of the scaffold is done

--- server/routes/gif.js
+++ server/routes/gif.js
@@ -5,8 +5,17 @@ router.get('/', function(req, res) {
   res.render("gif", {selected: '/gif', page: 'gif'});
 });
 
-router.get("/:id", function(req,res){
+router.get("/:photo", function(req,res){
+  var arithmageiaCharacters = ["rokuro","aonghus", "teza", "syna", "quinajah", "tertius"];
+  var requestedPhoto = req.params.photo;
+  console.log(requestedPhoto);
 
+  if(arithmageiaCharacters.indexOf(requestedPhoto) > -1){
+    var arithageiaImageUrl = "/images/arithmageia/"+ requestedPhoto + ".gif";
+
+    res.render("partial/gif/arithageia", {imgUrl: arithageiaImageUrl})
+  }
+  res.sendStatus(200);
 });
 
 module.exports = router;
