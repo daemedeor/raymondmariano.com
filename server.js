18bea95e032afefacc65cccec0ed0fa3d3027b41
Kingtak Wong <k.just.wong@gmail.com>
2015-10-12 23:46:35 -1000

    commiting a perfectly responsive and refactored main nav

--- server.js
+++ server.js
@@ -14,27 +14,27 @@ app.use(bodyParser.json());
 
 
 app.get("/contact", function(req,res){
-  res.render("contact", {selected: '/contact'});
+  res.render("contact", {selected: '/contact', page: 'contact'});
 });
 
 app.get("/about", function(req,res){
-  res.render("about", {selected: '/about'});
+  res.render("about", {selected: '/about', page: 'about'});
 });
 
 app.get("/videos", function(req,res){
-  res.render("videos", {selected: '/videos'});
+  res.render("videos", {selected: '/videos', page: 'videos'});
 });
 
-app.get("/gifs", function(req, res) {
-  res.render("gifs", {selected: '/gifs'});
+app.get("/gif", function(req, res) {
+  res.render("gif", {selected: '/gif', page: 'gif'});
 });
 
 app.get("/artportfolio", function(req, res) {
-  res.render("artportfolio", {selected: '/artportfolio'});
+  res.render("artportfolio", {selected: '/artportfolio', page: 'artportfolio'});
 });
 
 app.get("/", function(req, res) {
-  res.render("home", {selected: '/'});
+  res.render("home", {selected: '/', page: 'home'});
 });
 
 server.listen(app.get('port'), function(){
